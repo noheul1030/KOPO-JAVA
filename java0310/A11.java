be433eda56b8fac761c5c46e5d20e972aea8c268
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-03-17 08:27:01 +0900

    Add files via upload

--- java0310/A11.java
+++ java0310/A11.java
@@ -5,18 +5,58 @@ import java.util.Scanner;
 public class A11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		A11 a11 = new A11();
+		a11.result();
 	}
 
 	
-	public int scanner() {
+	public double scanner() {
 		Scanner scanner = new Scanner(System.in);
-		int input = scanner.nextInt();
-		return input;
+		return scanner.nextDouble();
+	}
+	
+	// m/s 값 받기
+	public double ms() {
+		double ms = this.scanner();
+		return ms;
+	}
+	// km/h 값 받기
+	public double kmh() {
+		double kmh = this.scanner() / 3.6;
+		
+		return kmh;
+	}
+	// m/ms 값 받기
+	public double mms() {
+		double mms = this.scanner() * 1000;
+		return mms;
+	}
+	
+	public void result() {
+		double ms = this.ms();
+		double kmh = this.kmh();
+		double mms = this.mms();
+		System.out.println();
+		
+		double big = 0;
+		
+		if(ms > mms) 
+			big = ms;
+		else if(mms > ms) 
+			big = mms;
+		else if(ms == mms)
+			big = ms;
+		
+		if(big < kmh)
+			big = kmh;
+			
+		if(big == ms) 
+			System.out.println((int)ms + "m/s");
+		else if(big == kmh)
+			System.out.println((int)(kmh * 3.6) +"km/h");
+		else if(big == mms)
+			System.out.println((int)(mms / 1000) +"m/ms");
+		
 	}
 }
 
-
-// 속도값 X 3.6
-// 1km = 0.277778초
